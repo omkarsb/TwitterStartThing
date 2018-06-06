5f6aba75ea3406b52aeff9e240d5362ed6a785f1
Omkar <omkarsb@gmail.com>
2018-06-05 17:07:48 -0700

    Twitter Timeline Module Added

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Input from "../presentational/Input";
+import { Timeline } from 'react-twitter-widgets';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
 class FormContainer extends Component {
   constructor() {
     super();
@@ -15,16 +18,36 @@ class FormContainer extends Component {
   render() {
     const { seo_title } = this.state;
     return (
-      <form id="article-form">
-        <Input
-          text="SEO title"
-          label="seo_title"
-          type="text"
-          id="seo_title"
-          value={seo_title}
-          handleChange={this.handleChange}
+        
+      <div>
+      <Tabs>
+        <TabList>
+          <Tab><h4>asd</h4></Tab>
+          <Tab><h4>TimeLine</h4></Tab>
+        </TabList>
+    
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>          
+          <Timeline
+            dataSource={{
+              sourceType: 'profile',
+              screenName: 'kimuchni_w485'
+            }}
+            options={{
+              username: 'kimuchni_w485',
+              height: '1000' 
+            }}
+            onLoad={() => console.log('Timeline is loaded!')}
         />
-      </form>
+</h2>
+        </TabPanel>
+      </Tabs>
+
+
+    </div>
     );
   }
 }
